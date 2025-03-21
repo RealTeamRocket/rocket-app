ca3a51470f20930652e236f3484647713904a596
Paul <jaul.ponas@gmail.com>
2025-03-21 14:45:06 +0100

    get uses from firestore db

--- mobile_app/lib/main.dart
+++ mobile_app/lib/main.dart
@@ -1,13 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
-
+import 'database/database.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp(
-    options: DefaultFirebaseOptions.currentPlatform,
-  );
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+
+  fetchUsers();
+
   runApp(const MyApp());
 }
 
