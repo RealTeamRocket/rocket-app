cd92a9bdfb6245fdd37a73936d041ea1fd4c23e3
dfgraeter <df.graeter@outlook.de>
2025-04-15 15:22:37 +0200

    route.dart Can now display a road that is also a loop. Distance can also be saved. Still problem because the distance will be printed two times
    -> Location needs to be always on. We need to redirect them to settings.
    -> One File with all background activity.
    -> How to sent the _routePoints into the DB and into the route.dart

--- mobile_app/lib/pages/route.dart
+++ mobile_app/lib/pages/route.dart
@@ -2,9 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:flutter_osm_plugin/flutter_osm_plugin.dart';
 import 'package:geolocator/geolocator.dart';
 
-import '../utils/tracking.dart';
-
-
 class RoutePage extends StatefulWidget {
   const RoutePage({super.key, required this.title});
 
@@ -30,21 +27,21 @@ Future<Position> _determinePosition() async {
   }
   if (locationPermission == LocationPermission.deniedForever) {
     return Future.error(
-        'Location permissions are permanently denied, we cannot request permissions.');
+      'Location permissions are permanently denied, we cannot request permissions.',
+    );
   }
   return await Geolocator.getCurrentPosition();
 }
 
 class _RoutePageState extends State<RoutePage> {
   GeoPoint? _initPosition;
-  final Tracking _tracking = Tracking();
-  List<GeoPoint> get _routePoints => _tracking.routePoints;
+  List<GeoPoint> _routePoints = [];
   late MapController _mapController;
-
+  var _distance = 0.0;
+  var numOfPoints = 0;
 
   @override
   void initState() {
-    _tracking.startTracking();
     super.initState();
     _setInitialPosition();
     _mapController = MapController(
@@ -56,7 +53,12 @@ class _RoutePageState extends State<RoutePage> {
     try {
       Position position = await _determinePosition();
       setState(() {
-
+        _initPosition = GeoPoint(latitude: 48.61313, longitude: 9.45881);
+        _routePoints = [
+          _initPosition!,
+          GeoPoint(latitude: 48.6156, longitude: 9.45984),
+          GeoPoint(latitude: 48.61651, longitude: 9.4549), // Beispielzielpunkt
+        ];
       });
     } catch (e) {
       // Handle the error accordingly
@@ -67,65 +69,102 @@ class _RoutePageState extends State<RoutePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: _initPosition == null
-          ? Center(child: CircularProgressIndicator())
-          : OSMFlutter(
-        controller: _mapController,
-        osmOption: OSMOption(
-          zoomOption: ZoomOption(
-            minZoomLevel: 5,
-            maxZoomLevel: 18,
-            initZoom: 16,
-          ),
-          userLocationMarker: UserLocationMaker(
-            personMarker: MarkerIcon(
-              icon: Icon(
-                Icons.location_on,
-                color: Colors.red,
-                size: 48,
+      appBar: AppBar(title: Text(widget.title)),
+      body:
+          _initPosition == null
+              ? Center(child: CircularProgressIndicator())
+              : OSMFlutter(
+                controller: _mapController,
+                osmOption: OSMOption(
+                  zoomOption: ZoomOption(
+                    minZoomLevel: 5,
+                    maxZoomLevel: 18,
+                    initZoom: 16,
+                  ),
+                  userLocationMarker: UserLocationMaker(
+                    personMarker: MarkerIcon(
+                      icon: Icon(
+                        Icons.location_on,
+                        color: Colors.red,
+                        size: 48,
+                      ),
+                    ),
+                    directionArrowMarker: MarkerIcon(
+                      icon: Icon(
+                        Icons.arrow_forward,
+                        color: Colors.blue,
+                        size: 48,
+                      ),
+                    ),
+                  ),
+                ),
+                mapIsLoading: Center(child: CircularProgressIndicator()),
+                onMapIsReady: (isReady) async {
+                  RoadInfo roadInfo;
+                  for(var point in _routePoints){
+                    /**
+                     * Checks if all points have been already used
+                     */
+                    if(numOfPoints == _routePoints.length){
+                      break;
+                    }
+                    numOfPoints++;
+                    /**
+                     * Draws the road between the last and first point
+                     */
+                    if(_routePoints.indexOf(point) == _routePoints.length-1){
+                        roadInfo = await _mapController.drawRoad(
+                        point,
+                        _routePoints[0],
+                        roadType: RoadType.foot,
+                        roadOption: RoadOption(
+                          roadColor: Colors.yellow,
+                          roadWidth: 8,
+                        ),
+                      );
+                       print("Rückweg: ${roadInfo.distance?.toStringAsFixed(4)} km");
+                      _distance += roadInfo.distance ?? 0;
+                      /**
+                       * Draws road between each other point 0->1, 1->2, ...
+                       */
+                    }else{
+                      roadInfo = await _mapController.drawRoad(
+                        point,
+                        _routePoints[_routePoints.indexOf(point) + 1],
+                        roadType: RoadType.foot,
+                        roadOption: RoadOption(
+                          roadColor: Colors.yellow,
+                          roadWidth: 8,
+                        ),
+                      );
+                      print(" ${roadInfo.distance?.toStringAsFixed(4)} km");
+                      /**
+                       * Adds the distance of the road to the total distance
+                       */
+                      _distance += roadInfo.distance ?? 0;
+                    }
+                  }
+                  /**
+                   * Here the distance will be saved into the database but there is still an inaccuracy
+                   * The whole distance gets printed two times.
+                   */
+                  print("Gesamte Distanz: ${_distance.toStringAsFixed(4)} km");
+                },
+                onGeoPointClicked: (geoPoint) {
+                  // Behandlung des Klicks auf einen Geopunkt, falls erforderlich
+                },
               ),
-            ),
-            directionArrowMarker: MarkerIcon(
-              icon: Icon(
-                Icons.arrow_forward,
-                color: Colors.blue,
-                size: 48,
-              ),
-            ),
-          ),
-        ),
-        mapIsLoading: Center(child: CircularProgressIndicator()),
-        onMapIsReady: (isReady) async {
-          if (isReady && _routePoints.length >= 2) {
-            _mapController.drawRoad(
-              _routePoints.first, // Startpunkt
-              _routePoints.last,  // Endpunkt
-              roadType: RoadType.foot,
-              intersectPoint: _routePoints.sublist(1, _routePoints.length - 1), // Zwischenpunkte
-              roadOption: RoadOption(
-                roadColor: Colors.yellow,
-                roadWidth: 10.0,
-              ),
-            );
-          }
-        },
-        onGeoPointClicked: (geoPoint) {
-          // Behandlung des Klicks auf einen Geopunkt, falls erforderlich
-        },
-      ),
+
     );
   }
 }
 
 void main() {
-  runApp(MaterialApp(
-    title: 'Route Page',
-    theme: ThemeData(
-      primarySwatch: Colors.blue,
+  runApp(
+    MaterialApp(
+      title: 'Route Page',
+      theme: ThemeData(primarySwatch: Colors.blue),
+      home: RoutePage(title: 'Route Page'),
     ),
-    home: RoutePage(title: 'Route Page'),
-  ));
+  );
 }
