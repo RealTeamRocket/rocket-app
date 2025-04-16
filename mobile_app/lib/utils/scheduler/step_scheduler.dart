0872729c95d0faa17d6f68374e0623884d7066a3
Paul <jaul.ponas@gmail.com>
2025-04-16 19:47:26 +0200

    working fetches

--- mobile_app/lib/utils/scheduler/step_scheduler.dart
+++ mobile_app/lib/utils/scheduler/step_scheduler.dart
@@ -1,5 +1,6 @@
 import 'package:background_fetch/background_fetch.dart';
 import 'package:flutter/cupertino.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
@@ -10,6 +11,12 @@ import '../backend_api/daily_steps_api.dart';
 void backgroundFetchHeadlessTask(HeadlessTask task) async {
   String taskId = task.taskId;
   bool isTimeout = task.timeout;
+  try {
+    await dotenv.load(fileName: ".env");
+  } catch (e) {
+    debugPrint("Error loading .env file using fallback: $e");
+  }
+  // Enable integration testing with the Flutter Driver extension.
   if (isTimeout) {
     // This task has exceeded its allowed running-time.
     // You must stop what you're doing and immediately .finish(taskId)
@@ -31,36 +38,47 @@ class StepScheduler {
 
   static Future<void> initialize() async {
     // Configure Background Fetch
-    await BackgroundFetch.configure(
-      BackgroundFetchConfig(
-        minimumFetchInterval: 15,
-        stopOnTerminate: false,
-        startOnBoot: true,
-        enableHeadless: true,
-        requiresBatteryNotLow: false,
-        requiresCharging: false,
-        requiresStorageNotLow: false,
-        requiresDeviceIdle: false,
-      ),
-      (String taskId) async {
-        debugPrint("[BackgroundFetch] Event received: $taskId at ${DateTime.now()}");
-        if (taskId == "flutter_background_fetch") {
-          try {
-            await sendData();
-          } catch (e) {
-            debugPrint("BackgroundFetch: Error in task: $e");
-          }
-        }
-        BackgroundFetch.finish(taskId);
-      },
-      (String taskId) async {
-        // <-- Task timeout handler.
-        // This task has exceeded its allowed running-time.  You must stop what you're doing and immediately .finish(taskId)
-        debugPrint("[BackgroundFetch] TASK TIMEOUT taskId: $taskId");
-        BackgroundFetch.finish(taskId);
-      },
-    );
+    try {
+      var status = await BackgroundFetch.configure(
+        BackgroundFetchConfig(
+          minimumFetchInterval: 15,
+          forceAlarmManager: false,
+          stopOnTerminate: false,
+          startOnBoot: true,
+          enableHeadless: true,
+          requiresBatteryNotLow: false,
+          requiresCharging: false,
+          requiresStorageNotLow: false,
+          requiresDeviceIdle: false,
+          requiredNetworkType: NetworkType.NONE,
+        ),
+        _onBackgroundFetch,
+        _onBackgroundFetchTimeout,
+      );
+      debugPrint('[BackgroundFetch] configure success: $status');
+    } on Exception catch (e) {
+      debugPrint("[BackgroundFetch] configure ERROR: $e");
+    }
   }
+}
+
+void _onBackgroundFetch(String taskId) async {
+  debugPrint("[BackgroundFetch] Event received: $taskId at ${DateTime.now()}");
+  if (taskId == "flutter_background_fetch") {
+    try {
+      await sendData();
+    } catch (e) {
+      debugPrint("BackgroundFetch: Error in task: $e");
+    }
+  }
+  BackgroundFetch.finish(taskId);
+}
+
+void _onBackgroundFetchTimeout(String taskId) {
+  // <-- Task timeout handler.
+  // This task has exceeded its allowed running-time.  You must stop what you're doing and immediately .finish(taskId)
+  debugPrint("[BackgroundFetch] TASK TIMEOUT taskId: $taskId");
+  BackgroundFetch.finish(taskId);
 }
 
 Future<void> sendData() async {
