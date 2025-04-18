81ae5404b382871b375a8660fdfa9d92ef5803e8
Paul <jaul.ponas@gmail.com>
2025-04-18 23:27:45 +0200

    tests for db calls

--- rocket-backend/integration-tests/database-tests/settings_table_test.go
+++ rocket-backend/integration-tests/database-tests/settings_table_test.go
@@ -2,9 +2,9 @@ package database_tests
 
 import (
 	"rocket-backend/internal/database"
-	// "rocket-backend/internal/types"
+	"rocket-backend/internal/types"
 
-	// "github.com/google/uuid"
+	"github.com/google/uuid"
 	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
 )
@@ -21,11 +21,69 @@ var _ = Describe("Settings table tests", func() {
 	AfterEach(func() {
 		_, err := srv.ExecuteRawSQL("DELETE FROM settings")
 		Expect(err).NotTo(HaveOccurred())
+		_, err = srv.ExecuteRawSQL("DELETE FROM image_store")
+		Expect(err).NotTo(HaveOccurred())
 	})
 
 	Context("Create Settings and retrieve them", func() {
+		It("Create Settings and get settings", func() {
+			id := uuid.New()
+			userID := uuid.New()
+			imageId := uuid.Nil
+			stepGoal := 1
+
+			settings := types.Settings {
+				ID: id,
+				UserId: userID,
+				ImageId: imageId,
+				StepGoal: stepGoal,
+			}
+
+			err := srv.CreateSettings(settings)
+			Expect(err).NotTo(HaveOccurred())
+
+			savedSettings, err := srv.GetSettingsByUserID(userID)
+			Expect(err).NotTo(HaveOccurred())
+
+			Expect(settings.ID).To(Equal(savedSettings.ID))
+			Expect(settings.UserId).To(Equal(savedSettings.UserId))
+			Expect(settings.ImageId).To(Equal(savedSettings.ImageId))
+			Expect(settings.StepGoal).To(Equal(savedSettings.StepGoal))
+		})
 	})
 
 	Context("Update Settings", func() {
+		It("Create Settings and get settings", func() {
+			id := uuid.New()
+			userID := uuid.New()
+			imageId := uuid.Nil
+			stepGoal := 1
+
+			settings := types.Settings {
+				ID: id,
+				UserId: userID,
+				ImageId: imageId,
+				StepGoal: stepGoal,
+			}
+
+			err := srv.CreateSettings(settings)
+			Expect(err).NotTo(HaveOccurred())
+
+			updatedSettings := types.SettingsDTO {
+				StepGoal: 100,
+			}
+			imageId = uuid.New()
+
+			err = srv.UpdateSettings(userID, updatedSettings, imageId)
+			Expect(err).NotTo(HaveOccurred())
+
+			savedSettings, err := srv.GetSettingsByUserID(userID)
+			Expect(err).NotTo(HaveOccurred())
+
+			Expect(settings.ID).To(Equal(savedSettings.ID))
+			Expect(settings.UserId).To(Equal(savedSettings.UserId))
+			Expect(updatedSettings.StepGoal).To(Equal(savedSettings.StepGoal))
+			Expect(imageId).To(Equal(savedSettings.ImageId))
+		})
 	})
 })
