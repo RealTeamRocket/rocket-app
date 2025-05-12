b71ed11ec28f5af9751d8f926846ec78d8928590
Pazl <130466427+Pazl27@users.noreply.github.com>
2025-05-12 19:25:46 +0200

    bootstrap + vue

--- website/src/views/HomeView.vue
+++ website/src/views/HomeView.vue
@@ -1,9 +1,14 @@
+<template>
+  <div class="container mt-5 text-center">
+    <h1>Welcome to the Homepage</h1>
+    <p>This is the home view of the application.</p>
+
+    <button class="btn btn-sm btn-success">Click Me! </button>
+  </div>
+</template>
+
 <script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
 </script>
 
-<template>
-  <main>
-    <TheWelcome />
-  </main>
-</template>
+<style scoped>
+</style>
