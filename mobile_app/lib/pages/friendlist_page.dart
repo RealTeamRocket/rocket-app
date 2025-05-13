04446deb29e96d02df3e092d0ac1fb72e99ac7f1
Manu <22homa1bif@hft-stuttgart.de>
2025-05-13 10:48:28 +0200

    removed onDispose()

--- mobile_app/lib/pages/friendlist_page.dart
+++ mobile_app/lib/pages/friendlist_page.dart
@@ -28,12 +28,6 @@ class _FriendlistPageState extends State<FriendlistPage> with TickerProviderStat
     loadData();
   }
 
-  @override
-  void dispose() {
-    _searchController.dispose();
-    super.dispose();
-  }
-
   Future<void> loadData() async {
     final jwt = await const FlutterSecureStorage().read(key: 'jwt_token');
     if (jwt == null) return;
