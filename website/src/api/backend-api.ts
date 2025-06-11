431c1bc4f06b2f87b715470bd401076e3d0fde5c
Manu <22homa1bif@hft-stuttgart.de>
2025-06-11 20:19:01 +0200

    apis for getting all users, adding friends, deleting friends

--- website/src/api/backend-api.ts
+++ website/src/api/backend-api.ts
@@ -48,11 +48,7 @@ export default {
     return protectedAxiosApi.delete(`/runs/${id}`, { withCredentials: true });
   },
   savePlannedRun(route: string, name: string, distance: number): Promise<AxiosResponse> {
-    return protectedAxiosApi.post(
-      '/runs/plan',
-      { route, name, distance },
-      { withCredentials: true }
-    );
+    return protectedAxiosApi.post('/runs/plan', { route, name, distance }, { withCredentials: true });
   },
   getPlannedRuns(): Promise<AxiosResponse> {
     return protectedAxiosApi.get('/runs/plan', { withCredentials: true });
@@ -60,20 +56,28 @@ export default {
   deletePlannedRun(id: string): Promise<AxiosResponse> {
     return protectedAxiosApi.delete(`/runs/plan/${id}`, { withCredentials: true });
   },
-    getChallenges(): Promise<AxiosResponse> {
-        return protectedAxiosApi.get('/challenges/new', { withCredentials: true });
-    },
-    completeChallenge(challengeId: string, rocketPoints: number): Promise<AxiosResponse> {
-        return protectedAxiosApi.post('/challenges/complete', { challenge_id: challengeId, rocket_points: rocketPoints }, { withCredentials: true });
-    },
-    getChallengeProgress(): Promise<AxiosResponse> {
-        return protectedAxiosApi.get('/challenges/progress', { withCredentials: true });
-    },
-    getFriends(): Promise<AxiosResponse> {
-        return protectedAxiosApi.get('/friends', { withCredentials: true });
-    },
-    inviteFriendToChallenge(challengeId: string, friendId: string): Promise<AxiosResponse> {
-        console.log("Inviting friend to challenge", challengeId, friendId);
-        return protectedAxiosApi.post('/challenges/invite', { challenge_id: challengeId, friend_id: friendId }, { withCredentials: true });
-    },
+  getChallenges(): Promise<AxiosResponse> {
+    return protectedAxiosApi.get('/challenges/new', { withCredentials: true });
+  },
+  completeChallenge(challengeId: string, rocketPoints: number): Promise<AxiosResponse> {
+    return protectedAxiosApi.post('/challenges/complete', { challenge_id: challengeId, rocket_points: rocketPoints }, { withCredentials: true });
+  },
+  getChallengeProgress(): Promise<AxiosResponse> {
+    return protectedAxiosApi.get('/challenges/progress', { withCredentials: true });
+  },
+  getFriends(): Promise<AxiosResponse> {
+    return protectedAxiosApi.get('/friends', { withCredentials: true });
+  },
+  inviteFriendToChallenge(challengeId: string, friendId: string): Promise<AxiosResponse> {
+    return protectedAxiosApi.post('/challenges/invite', { challenge_id: challengeId, friend_id: friendId }, { withCredentials: true });
+  },
+  addFriend(friendName: string): Promise<AxiosResponse> {
+    return protectedAxiosApi.post('/friends/add', { friend_name: friendName }, { withCredentials: true });
+  },
+  deleteFriend(friendName: string): Promise<AxiosResponse> {
+    return protectedAxiosApi.delete(`/friends/${encodeURIComponent(friendName)}`, { withCredentials: true });
+  },
+  getAllUsers(): Promise<AxiosResponse> {
+    return protectedAxiosApi.get('/users', { withCredentials: true });
+  },
 }
